7752cfe1a8811c12cc68e2c13d7a3924a55c3420
Sean McNamara <seanmcnamara33@gmail.com>
2022-04-01 12:35:31 -0700

    add functionality for style > [style] component, and style-circles component (there is no css-styling done, but functionality is there - needs event listners/handlers)

--- src/components/productOverview/Overview.jsx
+++ src/components/productOverview/Overview.jsx
@@ -3,10 +3,13 @@ import React, { useState, useEffect } from 'react';
 import StarReviews from './StarReviews.jsx';
 import ProductInfo from './ProductInfo.jsx';
 import IconList from './IconList.jsx';
+import StyleSelector from './StyleSelector.jsx';
+import StylesView from './StylesView.jsx';
 // state variables inside function?
 const Overview = () => {
   const [currentItem, setCurrentItem] = useState({});
   const [currentStyle, setCurrentStyle] = useState({});
+  const [allStyles, setAllStyles] = useState([]);
 
   const getFirstItem = () => {
     console.log(process.env.API_URI);
@@ -23,19 +26,20 @@ const Overview = () => {
   const getFirstStyle = (productId) => {
     fetch(`${process.env.API_URI}/products/${productId}/styles`, { method: 'GET', headers: { Authorization: process.env.API_KEY } })
       .then((response) => {
-        response.json().then((result) => setCurrentStyle(result.results[0]));
+        response.json().then((result) => {
+          setCurrentStyle(result.results[0]);
+          setAllStyles(result.results);
+        });
       })
       .catch((err) => {
         console.log(`Error found in getFirstStyle: ${err}`);
       });
   };
 
+
   const onReviewLinkClick = () => {
-    // either have Varun add a ref using useRef to the reviews header,
-    // so that you can use scrollIntoView on the ref to get to it on click
-    // OR, have Varun add an id to the review parent div,
-    // which you can then referene with document.getElementById(),
-    // and then you can call [element].scrollIntoView() on that element
+    // either have TIM add a ref using useRef to the reviews header, so that you can use scrollIntoView on the ref to get to it on click
+    // OR, have TIM add an id to the review parent div, which you can then referene with document.getElementById(), and then you can call [element].scrollIntoView() on that element
     // you can also make it scroll smoothly, with scrollIntoView({behavior: 'smooth'})
     console.log('onReviewLinkClick not ready yet!');
   };
@@ -46,7 +50,6 @@ const Overview = () => {
 
   useEffect(() => {
     if (Object.keys(currentItem).length) {
-      console.log(currentItem);
       getFirstStyle(currentItem.id);
     }
   }, [currentItem]);
@@ -69,6 +72,8 @@ const Overview = () => {
       </p>
       <ProductInfo currentItem={currentItem} />
       <IconList />
+      <StyleSelector currentItem={currentItem} currentStyle={currentStyle} />
+      <StylesView allStyles={allStyles} />
     </>
   );
 };
