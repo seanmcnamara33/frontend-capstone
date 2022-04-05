e401c52a643339b9f9c708b83413328337eec288
Sean McNamara <seanmcnamara33@gmail.com>
2022-04-05 16:18:17 -0700

    Create style circles, which each have the first image thumbnail of that color/style in them.

--- src/components/productOverview/styleSelector/StylesRow.jsx
+++ src/components/productOverview/styleSelector/StylesRow.jsx
@@ -1,5 +1,22 @@
 /* eslint-disable */
 import React from 'react';
+import styled from 'styled-components';
+
+const StyleCircleRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 60px;
+  margin: 10px;
+`;
+
+const StyleCircle = styled.img`
+  height: 60px;
+  width: 60px;
+  margin-left: 5px;
+  margin-right: 5px;
+  border: 1px solid black;
+  border-radius: 50%;
+`;
 
 const StylesRow = ({ index, styleGroup, onStyleCircleClick }) => {
   let trueStyleIndex;
@@ -8,12 +25,13 @@ const StylesRow = ({ index, styleGroup, onStyleCircleClick }) => {
   } else {
     trueStyleIndex = (index * 4);
   }
+  console.log(styleGroup);
   return (
-    <div className='styles-row'>
+    <StyleCircleRow className='styles-row'>
       {styleGroup.map((style, i) => {
-        return <span onClick={(event) => onStyleCircleClick(event, trueStyleIndex + i)} key={i} className='style-circle'>{style.name} | </span>
+        return <StyleCircle onClick={(event) => onStyleCircleClick(event, trueStyleIndex + i)} key={i} className='style-circle' src={style.photos[0].thumbnail_url}></StyleCircle>
       })}
-    </div>
+    </StyleCircleRow>
   );
 };
 
