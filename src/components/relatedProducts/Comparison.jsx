27ec1e22e9aa639b9432cd4230968284a432baeb
varunsrinivasan2 <varunsrinivasan2@gmail.com>
2022-04-08 14:21:41 -0700

    Modal fragment add

--- src/components/relatedProducts/Comparison.jsx
+++ src/components/relatedProducts/Comparison.jsx
@@ -4,7 +4,12 @@ import styled from 'styled-components';
 
 const Comparison = () => {
   return (
-    <div>hi</div>
+    <div>
+
+      <div>hi</div>
+      <button>x</button>
+
+    </div>
   )
 }
 
