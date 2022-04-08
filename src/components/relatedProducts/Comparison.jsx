0a45fe748fc689ec69c1618510a27e6646080f1c
varunsrinivasan2 <varunsrinivasan2@gmail.com>
2022-04-08 13:55:13 -0700

    Starting modal

---
+++ src/components/relatedProducts/Comparison.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Comparison = () => {
+  return (
+    <div>hi</div>
+  )
+}
+
+export default Comparison;
--- src/components/relatedProducts/RelatedList.jsx
+++ src/components/relatedProducts/RelatedList.jsx
@@ -19,7 +19,7 @@ const RelatedListStyle = styled.ul`
   list-style-type: none;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-around;
   height: 400px;
   width: auto;
   padding-left: 5px;
@@ -47,22 +47,22 @@ const RelatedList = ({ currentItem, id }) => {
   const [begin, setBegin] = useState(0);
   const [end, setEnd] = useState(3);
   const [display, setDisplay] = useState([]);
-  const productId = 65631;
-  
-  const getRelatedProductsID = () => {
+
+  const getRelatedProductsID = (productId) => {
     fetch(`${process.env.API_URI}/products/${productId}/related`, { method: 'GET', headers: { Authorization: process.env.API_KEY }})
-      .then((response) => {
-        response.json().then((results) => {
-          setRelatedProds([...new Set(results)]);
-          setDisplay([...new Set(results)]);
-        })
+      .then(response => response.json())
+      .then(results => {
+        setRelatedProds([...new Set(results)]);
+        setDisplay([...new Set(results)]);
       })
       .catch((err) => console.log(`Error getting related products: ${err}`))
   }
 
   useEffect(() => {
-    getRelatedProductsID();
-  }, [])
+    if (id) {
+      getRelatedProductsID(id);
+    }
+  }, [id])
 
   const rightClick = () => {
     if (begin === relatedProds.length - 3) {
@@ -98,7 +98,7 @@ const RelatedList = ({ currentItem, id }) => {
           );
         })}
       </RelatedListStyle>
-      {end < relatedProds.length && <ButtonRight onClick={rightClick}>&#187;</ButtonRight>}
+      {end < relatedProds.length && <ButtonRight onClick={rightClick}>&raquo;</ButtonRight>}
     </WidgetStyle>
     </div>
   );
