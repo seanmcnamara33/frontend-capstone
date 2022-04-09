e8ffbd67d982e5b480b763e8c57c6ccd46b8c9f3
Timothy <timnguy17@gmail.com>
2022-04-09 13:45:09 -0700

    added % of total reviews recommended

--- src/components/ratingsAndReviews/RatingsDisplay.jsx
+++ src/components/ratingsAndReviews/RatingsDisplay.jsx
@@ -1,14 +1,25 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import RatingsDisplayChart from './RatingsDisplayChart.jsx';
+import './RatingsDisplay.css';
 
 const RatingsDisplay = (props) => {
 
-  console.log(props.metaData.recommended);
+  console.log(props.metaData);
 
     const getRecValue = (props) => {
       let rec = props.metaData.recommended;
-      let recValue = Math.abs((+rec.false / +rec.true) * 100 -100);
+      let fval = 0;
+      let tval = 0;
+      for (let key in rec) {
+        if(key === 'false') {
+          fval = +rec[key]
+        }
+        if(key === 'true') {
+          tval = +rec[key]
+        }
+      }
+      let recValue = Math.abs((fval / tval) * 100 -100);
       return recValue;
     }
 
@@ -19,7 +30,10 @@ const RatingsDisplay = (props) => {
   return (
     <>
     <div>
-      {/* {revalue}% of reviews recommend this product */}
+      {/* //need average opf rating score here long with stars */}
+    </div>
+    <div className='review-recommend'>
+      {Math.round(getRecValue(props))}% of reviews recommend this product
     </div>
     <RatingsDisplayChart metaData={props.metaData.ratings}/>
     </>
