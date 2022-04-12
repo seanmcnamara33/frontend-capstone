b6f27d53daf1567dd671e3976711f823a8998bbc
Sean McNamara <seanmcnamara33@gmail.com>
2022-04-12 14:31:58 -0700

    fix issue 'clean up import #21'

--- src/components/productOverview/styleSelector/StylesRow.jsx
+++ src/components/productOverview/styleSelector/StylesRow.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import {BsCheck2Circle} from 'react-icons/bs';
 import {StyleCircleRow, StyleCircle, StyleCircleSelected} from './StyleSelectorStyles.jsx';
