080c24e4c5d2e249a80cdedb216d62fb991b062c
Timothy <timnguy17@gmail.com>
2022-04-12 13:36:45 -0700

    refactored helpfulness and uploading image functionality

--- src/components/ratingsAndReviews/ReviewList/Reviews.jsx
+++ src/components/ratingsAndReviews/ReviewList/Reviews.jsx
@@ -1,56 +1,36 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import './Reviews.css';
 import { formatDate } from '../../common/helpers.js';
 import ReviewPhotos from './ReviewPhotos.jsx';
 import ReviewBody from './ReviewBody.jsx';
+import ReviewHelpfulness from './ReviewHelpfulness.jsx';
+import StarsContainer from '../../common/StarsContainer.jsx';
+import {ProductContext} from '../../context/Product.jsx';
 
 
 
 const Reviews = (props) => {
+
+  const {currentItem} = useContext(ProductContext);
+
   return (
     props.data.map((review) => {
-      // const [helpfulness, setHelpfulness] = useState(review.helpfulness);
-
-      const handleYesClick = () => {
-        event.preventDefault();
-        fetch((`${process.env.API_URI}/reviews/${review.review_id}/helpful`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: process.env.API_KEY
-          }
-        })).then((response) => {
-          console.log('success helpfulness')
-          // setHelpfulness(review.helpfulness + 1)
-        }).catch((err) => {
-          console.log(err)
-        })
-      }
-
-      const handleReportClick = () => {
-        fetch((`${process.env.API_URI}/reviews/${review.review_id}/report`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: process.env.API_KEY
-          }
-        })).then((response) => {
-          console.log('success report')
-        }).catch((err) => {
-          console.log(err)
-        })
-      }
-
       return (
         <div className='review-tile' key={review.review_id}>
-          {review.rating}
+          {/* <StarsContainer currentItem={currentItem} starsAndReviews={false}/> */}
           <div className='review-summary'>{review.summary}</div>
-          <ReviewBody body={review.body} />
+          <div className='review-body'>
+            <ReviewBody body={review.body} />
+          </div>
           <ReviewPhotos data={review.photos} />
           <div className='review-name'>
-            By {review.reviewer_name}, {formatDate(review.date)} | Helpful? <a href="#" onClick={handleYesClick}>Yes</a>
-            ({review.helpfulness}) | <a href="#" onClick={handleReportClick}>{review.reported ? 'NO' : 'report'}</a>
+           <ReviewHelpfulness
+           name={review.reviewer_name}
+           date={review.date}
+           helpfulness={review.helpfulness}
+           review_id={review.review_id}
+           reported={review.reported}/>
           </div>
           <div>{review.email ? 'verified purchaser' : null}</div>
 
