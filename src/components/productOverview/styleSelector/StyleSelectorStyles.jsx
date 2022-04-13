b99e2a3ad28b7229a4af0788a95558d79bac48da
Sean McNamara <seanmcnamara33@gmail.com>
2022-04-13 14:39:41 -0700

    change styles for buttons - reverted back because react-select is really difficult to style, wont use again

--- src/components/productOverview/styleSelector/StyleSelectorStyles.jsx
+++ src/components/productOverview/styleSelector/StyleSelectorStyles.jsx
@@ -37,6 +37,10 @@ export const SelectSizing = styled.div`
   text-align: start;
 `;
 
+export const SelectPlaceholder = styled.div`
+  color: black;
+`;
+
 // Select Size Styles
 export const SizeDiv = styled.div`
   -webkit-appearance: none;
@@ -45,10 +49,12 @@ export const SizeDiv = styled.div`
   align-items: flex-end;
   max-height: 100%;
   width: 60%;
+  background-color: white;
+  color: black;
   padding:0.35em;
   border:0.1em solid black;
   margin:0 0.3em 0.3em 0;
-  border-radius: 3px;
+  border-radius: 10px;
   text-align: center;
   box-sizing: border-box;
   box-shadow: 0 0 2px black;
@@ -56,7 +62,7 @@ export const SizeDiv = styled.div`
   font-family:'Roboto',sans-serif;
   font-weight:300;
   &:hover {
-    box-shadow: 0 0 5px rgb(222, 99, 23);
+    box-shadow: 0 0 5px rgb(59, 167, 184);
   }
 `;
 
@@ -70,7 +76,7 @@ export const QuantityDiv = styled.div`
   padding:0.35em;
   border:0.1em solid black;
   margin:0 0.3em 0.3em 0;
-  border-radius: 3px;
+  border-radius: 10px;
   text-align: center;
   box-sizing: border-box;
   box-shadow: 0 0 2px black;
@@ -78,6 +84,6 @@ export const QuantityDiv = styled.div`
   font-family:'Roboto',sans-serif;
   font-weight:300;
   &:hover {
-    box-shadow: 0 0 5px rgb(222, 99, 23);
+    box-shadow: 0 0 5px rgb(59, 167, 184);
   }
 `;
